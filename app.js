983b055e404a2a9b18693dcfa780bca5dcf95820
James Feore <jjfeore@gmail.com>
2018-04-02 21:02:37 -0700

    Updated to not get too many requests

--- app.js
+++ app.js
@@ -1,79 +1,26 @@
 'use strict';
 
 function drawPage() {
-  setInterval(checkBanzai, 10000);
-  setInterval(checkWavy, 10000);
-  setInterval(checkRyann, 10000);
-  setInterval(checkMira, 10000);
+  setInterval(checkOnline, 10000);
 }
 
-function checkBanzai() {
+function checkOnline() {
+  let streamerDict = {};
+  streamerDict[102090784] = '#banzai-img';
+  streamerDict[62334719] = '#ryann-img';
+  streamerDict[81578595] = '#wavy-img';
+  streamerDict[85919636] = '#mira-img';
+
   $.ajax({
-    url: 'https://api.twitch.tv/helix/streams?user_login=banzaibaby',
+    url: 'https://api.twitch.tv/helix/streams?user_login=banzaibaby&user_login=wavy&user_login=theonlyryann&user_login=xmiramira',
     headers: {
       'Client-ID': '6mrlp3nz5f0bitqfio2p4u8r28mtmf'
     },
     type: 'GET',
     success: function(result) {
-      if (result.data.length > 0) {
-        $('#banzai-img').css('box-shadow', '0 0 6px 6px #f00');
-      }
-      else {
-        $('#banzai-img').css('box-shadow', '0 0 0 0 #0f0');
-      }
-    }
-  });
-}
-
-function checkMira() {
-  $.ajax({
-    url: 'https://api.twitch.tv/helix/streams?user_login=xmiramira',
-    headers: {
-      'Client-ID': '6mrlp3nz5f0bitqfio2p4u8r28mtmf'
-    },
-    type: 'GET',
-    success: function(result) {
-      if (result.data.length > 0) {
-        $('#mira-img').css('box-shadow', '0 0 6px 6px #f00');
-      }
-      else {
-        $('#mira-img').css('box-shadow', '0 0 0 0 #0f0');
-      }
-    }
-  });
-}
-
-function checkRyann() {
-  $.ajax({
-    url: 'https://api.twitch.tv/helix/streams?user_login=theonlyryann',
-    headers: {
-      'Client-ID': '6mrlp3nz5f0bitqfio2p4u8r28mtmf'
-    },
-    type: 'GET',
-    success: function(result) {
-      if (result.data.length > 0) {
-        $('#ryann-img').css('box-shadow', '0 0 6px 6px #f00');
-      }
-      else {
-        $('#ryann-img').css('box-shadow', '0 0 0 0 #0f0');
-      }
-    }
-  });
-}
-
-function checkWavy() {
-  $.ajax({
-    url: 'https://api.twitch.tv/helix/streams?user_login=wavy',
-    headers: {
-      'Client-ID': '6mrlp3nz5f0bitqfio2p4u8r28mtmf'
-    },
-    type: 'GET',
-    success: function(result) {
-      if (result.data.length > 0) {
-        $('#wavy-img').css('box-shadow', '0 0 6px 6px #f00');
-      }
-      else {
-        $('#wavy-img').css('box-shadow', '0 0 0 0 #0f0');
+      $('.avatars').css('box-shadow', '0 0 0 0 #0f0');
+      for (let streamer of result.data) {
+        $(streamerDict[streamer.user_id]).css('box-shadow', '0 0 6px 6px #f00');
       }
     }
   });
